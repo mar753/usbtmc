8db2052d71091abd2fb63cb9cf0cce9d16fa8185
Marcel <5397997+mar753@users.noreply.github.com>
2021-04-22 10:19:05 +0200

    Convention updates

--- automatedacquisition.py
+++ automatedacquisition.py
@@ -23,65 +23,65 @@ class AutomatedAcquisition:
 
     ## The constructor.
     # Initializes values and connects to the first USBTMC device found.
-    # @param numberOfTraces Number of traces we would like to acquire.
-    # @param initSettingsCallback Callback with additional settings for
+    # @param number_of_traces Number of traces we would like to acquire.
+    # @param init_settings_callback Callback with additional settings for
     # the oscilloscope we would like to apply.
-    def __init__(self, numberOfTraces, initSettingsCallback):
-        self._initSuccessful = True
-        self._numberOfTraces = numberOfTraces
+    def __init__(self, number_of_traces, init_settings_callback):
+        self._init_successful = True
+        self._number_of_traces = number_of_traces
         rm = visa.ResourceManager()
         devices = rm.list_resources()
         pattern = re.compile("USB[0-9]+")
-        usbList = list(filter(pattern.match, devices))
-        if (len(usbList) <= 0):
+        usb_list = list(filter(pattern.match, devices))
+        if (len(usb_list) <= 0):
             print("No valid USB device found")
-            self._initSuccessful = False
+            self._init_successful = False
             return
 
         # Allocate memory (8-bit per sample) for:
         # 1048576 samples + 10 header (1M points fetch);
         # timeout set to 10s (about 6s per one 1M samples fetch)
-        self._device = rm.open_resource(usbList[0], timeout=10000, chunk_size=1048586)
-        self.__resetSettings(initSettingsCallback)
+        self._device = rm.open_resource(usb_list[0], timeout=10000, chunk_size=1048586)
+        self.__reset_settings(init_settings_callback)
 
     ## Begin capturing method.
-    def startCapturing(self):
-        if (self._initSuccessful == False):
+    def start_capturing(self):
+        if (self._init_successful == False):
             return
-        ch2State = self._device.query(":CHAN2:DISP?")
+        ch2_state = self._device.query(":CHAN2:DISP?")
         self.__write(":RUN")
 
-        for i in range(0, self._numberOfTraces):
-            self.__waitForTriggerToBeFired()
-            self.__channelAcquire(1, i)
-            if (ch2State == "1"):
-                self.__channelAcquire(2, i)
+        for i in range(0, self._number_of_traces):
+            self.__wait_for_trigger_to_be_fired()
+            self.__channel_acquire(1, i)
+            if (ch2_state == "1"):
+                self.__channel_acquire(2, i)
             self.__write(":RUN")
-        self.closeConnection()
+        self.close_connection()
 
     ## Close connection to the device.
-    def closeConnection(self):
-        if (self._initSuccessful == False):
+    def close_connection(self):
+        if (self._init_successful == False):
             return
         self.__write(":KEY:FORCE") # Disables 'Rmt' mode in Rigol
         self._device.close()
 
-    def __channelAcquire(self, channelNumber, i):
-        self.__write(":WAV:DATA? CHAN" + str(channelNumber))
-        channelRawData = self._device.read_raw()
-        channelRawData = channelRawData[10:] # Cut header
-        print("CH" + str(channelNumber) + " data size:", len(channelRawData))
-        f = open("dataCH" + str(channelNumber) + "_" + str(i) + ".txt", "wb")
-        f.write(channelRawData)
+    def __channel_acquire(self, channel_number, i):
+        self.__write(":WAV:DATA? CHAN" + str(channel_number))
+        channel_raw_data = self._device.read_raw()
+        channel_raw_data = channel_raw_data[10:] # Cut header
+        print("CH" + str(channel_number) + " data size:", len(channel_raw_data))
+        f = open("dataCH" + str(channel_number) + "_" + str(i) + ".txt", "wb")
+        f.write(channel_raw_data)
         f.close()
 
-    def __resetSettings(self, initSettingsCallback):
+    def __reset_settings(self, init_settings_callback):
         self.__write(":STOP")
         self.__write(":ACQ:MEMD LONG") # Memory depth: long memory
         self.__write(":WAV:POIN:MODE RAW") # Get raw samples
-        initSettingsCallback(self.__write)
+        init_settings_callback(self.__write)
 
-    def __waitForTriggerToBeFired(self):
+    def __wait_for_trigger_to_be_fired(self):
         time.sleep(.1)
         trigger = self._device.query(":TRIG:STAT?")
         while (trigger == "WAIT"):
