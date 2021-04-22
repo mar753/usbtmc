47948f655e652db5191e252f4e655cad34b729a8
Marcel <5397997+mar753@users.noreply.github.com>
2021-04-22 10:21:11 +0200

    Update rigol_acquire.py

--- rigol_acquire.py
+++ rigol_acquire.py
@@ -5,27 +5,27 @@
 
 import automatedacquisition
 
-def initSettings(writeHandle):
-    writeHandle(':CHAN1:BWL OFF') # CH1 disable BW limit
-    writeHandle(':CHAN1:COUP DC') # CH1 DC coupling
-    writeHandle(':CHAN1:DISP ON') # CH1 enable
-    writeHandle(':CHAN1:INV OFF') # CH1 disable invert
-    writeHandle(':CHAN1:SCAL .05') # CH1 vertical scale 50mV
-    writeHandle(':CHAN1:OFFS -0.1') # CH1 offset -100mV
-    writeHandle(':CHAN2:BWL OFF')
-    writeHandle(':CHAN2:COUP DC')
-    writeHandle(':CHAN2:DISP OFF') # CH2 disable
-    writeHandle(':CHAN2:INV OFF')
-    writeHandle(':CHAN2:SCAL 1')
-    writeHandle(':CHAN2:OFFS -2')
-    writeHandle(':TIM:SCAL .01') # Time scale 10ms
-    writeHandle(':TIM:OFFS 0') # Time offset 0
-    writeHandle(':TRIG:MODE EDGE') # Trigger on edge
-    writeHandle(':TRIG:EDGE:SOUR CHAN1') # CH1 trigger source
-    writeHandle(':TRIG:EDGE:SWE SING') # Sweep single
-    writeHandle(':TRIG:EDGE:COUP DC') # Trigger DC coupling
-    writeHandle(':TRIG:EDGE:SLOP POS') # Trigger on rising edge
-    writeHandle(':TRIG:EDGE:LEV .08') # Trigger level 80mV
+def init_settings(write_handle):
+    write_handle(':CHAN1:BWL OFF') # CH1 disable BW limit
+    write_handle(':CHAN1:COUP DC') # CH1 DC coupling
+    write_handle(':CHAN1:DISP ON') # CH1 enable
+    write_handle(':CHAN1:INV OFF') # CH1 disable invert
+    write_handle(':CHAN1:SCAL .05') # CH1 vertical scale 50mV
+    write_handle(':CHAN1:OFFS -0.1') # CH1 offset -100mV
+    write_handle(':CHAN2:BWL OFF')
+    write_handle(':CHAN2:COUP DC')
+    write_handle(':CHAN2:DISP OFF') # CH2 disable
+    write_handle(':CHAN2:INV OFF')
+    write_handle(':CHAN2:SCAL 1')
+    write_handle(':CHAN2:OFFS -2')
+    write_handle(':TIM:SCAL .01') # Time scale 10ms
+    write_handle(':TIM:OFFS 0') # Time offset 0
+    write_handle(':TRIG:MODE EDGE') # Trigger on edge
+    write_handle(':TRIG:EDGE:SOUR CHAN1') # CH1 trigger source
+    write_handle(':TRIG:EDGE:SWE SING') # Sweep single
+    write_handle(':TRIG:EDGE:COUP DC') # Trigger DC coupling
+    write_handle(':TRIG:EDGE:SLOP POS') # Trigger on rising edge
+    write_handle(':TRIG:EDGE:LEV .08') # Trigger level 80mV
 
-aa = automatedacquisition.AutomatedAcquisition(3, initSettings) # 3 traces
-aa.startCapturing()
+aa = automatedacquisition.AutomatedAcquisition(3, init_settings) # 3 traces
+aa.start_capturing()
